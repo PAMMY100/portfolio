19b2beb20e65196df838e6eafcbe828541efcad4
PAMMY100 <princesamuel636@gmail.com>
2025-09-30 06:40:35 +0100

    quick changes

--- src/components/Testing.tsx
+++ src/components/Testing.tsx
@@ -9,7 +9,7 @@ import Image from 'next/image'
 const Projects = () => {
 
   return (
-    <div className='text-[#90A1B9] grid gap-6 sm:grid-cols-2 lg:grid-cols-3 p-10'>
+    <div className='p-5 text-[#90A1B9] grid gap-6 sm:grid-cols-2 lg:grid-cols-3 md:p-14'>
         {
             testingProject.map((data) => (
                 <div key={data.name} className='bg-[#020618] rounded-2xl overflow-hidden shadow-lg'>
